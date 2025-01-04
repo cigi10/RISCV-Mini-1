a67b88ea30ccd4800d54544e11f5ac69d7d0df65
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2025-01-04 21:21:28 +0530

    Update PC_plus4_Adder.sv

--- Design_Sources/PC_plus4_Adder.sv
+++ Design_Sources/PC_plus4_Adder.sv
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
-
 //////////////////////////////////////////////////////////////////////////////////
-
 module PC_plus4_Adder(input logic [31:0] fromPC, output logic [31:0] nexttoPC);
 assign nexttoPC = 4 + fromPC;
 endmodule
